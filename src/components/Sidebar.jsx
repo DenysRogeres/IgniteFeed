35b03fe3ac5f14f8cc0d9b3d88d4b9b26a84440b
Denys Rógeres <dennysrogeres@gmail.com>
2022-06-16 11:50:50 -0300

    adicionando componente post e suas estilizações

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,15 @@
 
 import { PencilLine } from 'phosphor-react';
-import style from './Sidebar.module.css';
+import styles from './Sidebar.module.css';
 
 export function Sidebar() {
     return (
-        <aside className={style.sidebar}>
-            <img className={style.cover} src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=50"/>
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=50"/>
         
 
-            <div className={style.profile}>
-                <img className={style.avatar} src="https://github.com/DenysRogeres.png" alt="Foto de perfil" />
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/DenysRogeres.png"/>
 
                 <strong>Denys Rógeres</strong>
                 <span>Engenheiro de Software</span>
