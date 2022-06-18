a0efa6db090b32418ec6aa77deff665576bbe1b3
Denys Rógeres <dennysrogeres@gmail.com>
2022-06-18 13:53:51 -0300

    componentizando avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,13 @@
-import { Comment } from './Comment'
-import styles from './Post.module.css'
+import { Comment } from './Comment';
+import { Avatar } from './Avatar';
+import styles from './Post.module.css';
 
 export function Post() {
     return (
         <article className={styles.post}>
            <header>
                <div className={styles.author}>
-                   <img className={styles.avatar} src="https://github.com/DenysRogeres.png" />
+                    <Avatar src="https://github.com/DenysRogeres.png" />
                     <div className={styles.authorInfo}>
                         <strong>Denys Rógeres</strong>
                         <span>Engenheiro de Software</span>
